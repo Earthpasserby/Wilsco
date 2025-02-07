737ddf903fd26cac945cf5826504420d050746b4
Earthpasserby <iwuno68@gmail.com>
2025-02-07 12:29:05 +0100

    m

---
+++ pages/Faqs.jsx
@@ -0,0 +1,15 @@
+import Bfooter from "../components/Bfooter";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+export const Faqs = () => {
+  return (
+    <>
+      <div className="w-full h-full">
+        <Header />
+        <Bfooter />
+        <Footer />
+      </div>
+    </>
+  );
+};
