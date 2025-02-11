8cad0601a37f8be90f203058ef625ce5abb001d5
Earthpasserby <iwuno68@gmail.com>
2025-02-11 02:31:07 +0100

    fixes

--- components/Faqsdet.jsx
+++ components/Faqsdet.jsx
@@ -29,8 +29,66 @@ const faqSets = {
       question: "Can I apply if I’m not employed?",
       answer: "Yes, you can. You can make use of a sponsor.",
     },
+    {
+      question: "Is the submission online or physical?",
+      answer:
+        "It's an online submission, however, you will be required to go for biometrics at either the Ikeja, VI or Abuja centre.",
+    },
+    {
+      question: "Is the application interview based?",
+      answer: "No, it's not. You will be required to go for biometrics.",
+    },
+    {
+      question:
+        "Will I be able to go for biometrics and submission immediately I make payment and apply?",
+      answer:
+        "Submission is based on the available appointment date. Please note that the appointment date availability changes from time to time.",
+    },
+    {
+      question: "Is an invitation letter compulsory for the visa application?",
+      answer:
+        "No, an invitation letter is not compulsory but can be an added advantage. However, it does not guarantee visa issuance.",
+    },
+    {
+      question: "How much should be in my account while applying for the visa?",
+      answer:
+        "You should have enough to cover for your trip. Our expert agents will give you further guidance.",
+    },
+    {
+      question: "What is the best time to reapply for a visa after denial?",
+      answer: "You can reapply immediately with expert assistance.",
+    },
+    {
+      question:
+        "Do I have to come to your office before I start my application?",
+      answer:
+        "It's not compulsory to come to the office, everything can be done online via mail. However, you're welcome to verify our office and speak with any of our agents.",
+    },
+    {
+      question: "What is the guarantee of getting a visa?",
+      answer:
+        "We offer 95% guarantee on our visa applications. With our expertise and wide knowledge , we will guide you for a higher chance of getting your visa, however, visa issuance is at the discretion of the embassy consulate.",
+    },
+    {
+      question: "Will I be refunded after a visa denial?",
+      answer:
+        "No, you will not be refunded but you can reapply without paying the visa processing fee. (This is only valid for 3 months after denial)",
+    },
+    {
+      question: "Can I travel immediately when my visa is out?",
+      answer: "Yes, you can",
+    },
+    {
+      question: "Is it compulsory to have a travel history before I apply?",
+      answer: " No, it's not compulsory. It is just an added advantage.",
+    },
+    {
+      question:
+        "Can Willsco handle my flight and accommodation bookings when my visa is out?",
+      answer: "Of course we can and at affordable rates too!!!",
+    },
   ],
-  travel: [
+  school: [
     {
       question: "What should I pack for international travel?",
       answer: "Passport, visa, clothes, money, etc.",
@@ -80,7 +138,7 @@ export default function Faqsdet() {
   const [selectedFaq, setSelectedFaq] = useState("visa"); // Tracks current FAQ category
   const [openIndex, setOpenIndex] = useState(null); // Tracks open question
   const [currentPage, setCurrentPage] = useState(1); // Tracks pagination
-  const itemsPerPage = 2; // Number of questions per page
+  const itemsPerPage = 7; // Number of questions per page
 
   const toggleFAQ = (index) => {
     setOpenIndex(openIndex === index ? null : index);
@@ -99,7 +157,7 @@ export default function Faqsdet() {
       <div className="max-w-4xl mx-auto p-6">
         {/* FAQ Category Buttons */}
         <div className="w-full flex gap-4  rounded-2xl py-4 px-4">
-          {["visa", "travel", "study"].map((type) => (
+          {["visa", "school", "study"].map((type) => (
             <button
               key={type}
               className={`px-4 py-2 rounded-lg ${
@@ -113,8 +171,8 @@ export default function Faqsdet() {
             >
               {type === "visa"
                 ? "Visa Services"
-                : type === "School Service"
-                ? "Travel Tips"
+                : type === "school"
+                ? "School service"
                 : "Study Abroad"}
             </button>
           ))}
