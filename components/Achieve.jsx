2b1266c2c2b6daa5511acccd6ae010041a36e58b
Earthpasserby <iwuno68@gmail.com>
2025-02-12 16:58:02 +0100

    fix

--- components/Achieve.jsx
+++ components/Achieve.jsx
@@ -1,91 +1,91 @@
 const Achieve = () => {
   return (
     <>
-      <div className="flex justify-center bg-[#fbfbfb] ">
-        <div className="sm:px-14 lg:px-14 px-4 w-full max-w-screen-xl">
-          <h4 className="text-[20px] font-normal text-[#1E1E1E] pt-14 text-center">
+      <div className="">
+        <div className="sm:px-14 lg:px-14 px-4 bg-[#fbfbfb]">
+          <h4 className="text-[20px] font-normal text-[#1E1E1E] pt-14">
             Achieve your dream in 5 simple steps
           </h4>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 pt-10 gap-4">
-            <div className="bg-white rounded-2xl px-10 flex flex-col items-center w-full">
-              <div className="pt-8 mb-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 pt-10 gap-4">
+            <div className="bg-white rounded-2xl px-8">
+              <div className="pt-8  mb-8 ">
                 <a
                   href="#"
-                  className="bg-red-700 text-white px-4 py-2 rounded-4xl"
+                  className="bg-red-700  text-white px-4 py-2 rounded-4xl"
                 >
                   Step 01
                 </a>
               </div>
-              <h5 className="font-medium text-[20px] text-[#1E1E1E] text-center">
+              <h5 className="font-medium text-[20px] text-[#1E1E1E]">
                 Consultation & Assessment
               </h5>
-              <p className="text-[#686868] text-[16px] font-normal mb-8 text-center">
+              <p className="text-[#686868] text-[16px] font-normal mb-8">
                 Book a consultation to discover the best study options tailored
                 to you.
               </p>
             </div>
-            <div className="bg-white rounded-2xl px-10 flex flex-col items-center w-full">
-              <div className="pt-8 mb-8">
+            <div className="bg-white rounded-2xl px-8">
+              <div className="pt-8  mb-8 ">
                 <a
                   href="#"
-                  className="bg-red-700 text-white px-4 py-2 rounded-4xl"
+                  className="bg-red-700  text-white px-4 py-2 rounded-4xl"
                 >
                   Step 02
                 </a>
               </div>
-              <h5 className="font-medium text-[20px] text-[#1E1E1E] text-center">
+              <h5 className="font-medium text-[20px] text-[#1E1E1E]">
                 Secure Your Application
               </h5>
-              <p className="text-[#686868] text-[16px] font-normal mb-8 text-center">
+              <p className="text-[#686868] text-[16px] font-normal mb-8">
                 Make payment to kick-start your school search and application
                 process.
               </p>
             </div>
-            <div className="bg-white rounded-2xl px-10 flex flex-col items-center w-full">
-              <div className="pt-8 mb-8">
+            <div className="bg-white rounded-2xl px-8">
+              <div className="pt-8  mb-8 ">
                 <a
                   href="#"
-                  className="bg-red-700 text-white px-4 py-2 rounded-4xl"
+                  className="bg-red-700  text-white px-4 py-2 rounded-4xl"
                 >
                   Step 03
                 </a>
               </div>
-              <h5 className="font-medium text-[20px] text-[#1E1E1E] text-center">
+              <h5 className="font-medium text-[20px] text-[#1E1E1E]">
                 Admission <br /> Success
               </h5>
-              <p className="text-[#686868] text-[16px] font-normal mb-8 text-center">
+              <p className="text-[#686868] text-[16px] font-normal mb-8">
                 Receive your admission letter from your preferred university.
               </p>
             </div>
-            <div className="bg-white rounded-2xl px-10 flex flex-col items-center w-full">
-              <div className="pt-8 mb-8">
+            <div className="bg-white rounded-2xl px-8">
+              <div className="pt-8  mb-8 ">
                 <a
                   href="#"
-                  className="bg-red-700 text-white px-4 py-2 rounded-4xl"
+                  className="bg-red-700  text-white px-4 py-2 rounded-4xl"
                 >
                   Step 04
                 </a>
               </div>
-              <h5 className="font-medium text-[20px] text-[#1E1E1E] text-center">
+              <h5 className="font-medium text-[20px] text-[#1E1E1E]">
                 Study Permit Application
               </h5>
-              <p className="text-[#686868] text-[16px] font-normal mb-8 text-center">
+              <p className="text-[#686868] text-[16px] font-normal mb-8">
                 Apply for your study permit with expert guidance.
               </p>
             </div>
-            <div className="bg-white rounded-2xl px-10 flex flex-col items-center w-full">
-              <div className="pt-8 mb-8">
+            <div className="bg-white rounded-2xl px-8">
+              <div className="pt-8  mb-8 ">
                 <a
                   href="#"
-                  className="bg-red-700 text-white px-4 py-2 rounded-4xl"
+                  className="bg-red-700  text-white px-4 py-2 rounded-4xl"
                 >
                   Step 05
                 </a>
               </div>
-              <h5 className="font-medium text-[20px] text-[#1E1E1E] text-center">
+              <h5 className="font-medium text-[20px] text-[#1E1E1E]">
                 Start Your <br /> Journey
               </h5>
-              <p className="text-[#686868] text-[16px] font-normal mb-8 text-center">
+              <p className="text-[#686868] text-[16px] font-normal mb-8">
                 Pack your bags—you’re on your way to achieving your dream of
                 studying abroad!
               </p>
