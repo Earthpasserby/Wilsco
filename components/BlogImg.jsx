b95a66bcdcb4239b66c5e7e2cd5a3955e3e97570
tradershor <allisonmiller676@gmail.com>
2025-02-12 23:12:33 +0100

    mm

--- components/BlogImg.jsx
+++ components/BlogImg.jsx
@@ -5,7 +5,7 @@ export const BlogImg = () => {
         <div className="absolute inset-0 bg-black opacity-70"></div>
         <div className="absolute inset-0 top-[50vh] sm:top-[30vh] md:top-[50vh] lg:top-[60vh]  sm:px-14 lg:px-14 px-4">
           <div>
-            <span className="font-medium sm:text-[40px] text-white">
+            <span className="font-medium sm:text-[35px] text-white">
               The ultimate guide to studying abroad:
               <br /> Everything you need to know
             </span>
@@ -13,7 +13,7 @@ export const BlogImg = () => {
               Discover the step-by-step process of studying in top destinations,
               from choosing the right school to securing your visa.
             </p>
-            <div className="pt-6">
+            <div className="pt-4">
               <button className="bg-red-600 text-white px-8 py-3 rounded-4xl hover:bg-red-700 flex items-center space-x-2">
                 <span>Read</span>
                 {/* <FaSearch /> */}
