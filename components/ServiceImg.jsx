006b8c6159321667fa2483f63bb40c7a3f0af1b0
tradershor <allisonmiller676@gmail.com>
2025-02-13 14:21:52 +0100

    modifications

--- components/ServiceImg.jsx
+++ components/ServiceImg.jsx
@@ -5,12 +5,12 @@ const ServiceImg = () => {
         <div className="relative h-[50vh] sm:h-[40vh] md:h-[50vh] lg:h-[100vh]   ">
           <div className="bg-[url('/images/fiend.png')] bg-cover bg-center h-full">
             <div className="absolute inset-0 bg-black opacity-70"></div>
-            <div className="absolute inset-0 top-[20vh] sm:top-[30vh] md:top-[50vh] lg:top-[70vh]  sm:px-14 lg:px-14 px-4 ">
+            <div className="absolute inset-0 top-[25vh] sm:top-[30vh] md:top-[50vh] lg:top-[70vh]  sm:px-14 lg:px-14 px-4 ">
               <div>
-                <span className="font-medium text-[32px] text-white">
+                <span className="font-medium sm:text-[32px] text-white">
                   SCHOOL SERVICE
                 </span>
-                <p className="sm-w-[567px] lg:w-[567px] text-[16px] font-normal text-white leading-5 ">
+                <p className="sm-w-[567px] lg:w-[567px] sm:text-[16px] font-normal text-white leading-5 ">
                   We can assist you with reducing the stress associated with
                   University application processes. We make applying to
                   Universities hassle free, quick and simple. With our
